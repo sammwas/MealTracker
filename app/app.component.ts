b18bbaf5c14a441bfb01f50ed508046d141f5990
sammwas <samson.m.njuguna@gmail.com>
2017-07-08 15:12:52 +0300

    changed the whole app component

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,103 +3,57 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <div class="container">
-    <h2>Meal Tracker</h2>
-    <h3>Breakfast</h3>
+  <div class="container-fluid">
 
+    <h1>Meal Tracker App</h1>
+  <div class="col-md-6">
+  <div class="menuForm" *ngFor="let meal of meals">
 
+    <h3> {{ meal.name }}</h3>
+    <button (click)="showDetails(meal)">Submit</button>
+  </div>
+  </div>
 
-    <form>
-
-      <label>Name:</label>
-      <input type="text"  #breakfastname/>
-      <label>Details:</label>
-      <input type="text" #breakfastdet/>
-      <label>Calories:</label>
-      <input type="text" #breakfastcal/>
-      <button (click)='addName(breakfastname.value,breakfastdet.value,breakfastcal.value)'>submit</button>
-
-   </form>
-
-    <hr/>
-
-    <h3>Lunch</h3>
+    <div class="col-md-6">
+    <div *ngIf="selectedFood" class="col-md-6">
+      <h1>Edit Form</h1>
       <div>
-      <form>
-        <label>Name:</label>
-        <input type="text" #lunchname>
-        <label>Details:</label>
-        <input type="text" #lunchdet>
-        <label>Calories:</label>
-        <input type="text" #lunchcalc>
-        <button (click)='addLunch(lunchname.value,lunchdet.value,lunchcalc.value)'>submit</button>
-        </form>
-      </div>
-    <hr/>
-
-    <h3>Diner</h3>
-      <div>
-      <form>
-        <label>Name:</label>
-        <input type="text" #dinername>
-        <label>Details:</label>
-        <input type="text" #dinerdet>
-        <label>Calories:</label>
-        <input type="text" #dinnercalc>
-        <button (click)='addDinner(dinername.value,dinerdet.value,dinnercalc.value)'>submit</button>
-        </form>
+      <label>Enter Food Name:</label>
+      <input [(ngModel)]="selectedFood.name">
     </div>
-
     <div>
-    <h3>Breakfast</h3>
-     <ul *ngFor='let meal of breakfast'>
-       <li>{{meal.name}}</li>
-       <li>{{meal.details}}</li>
-       <li>{{meal.calories}}</li>
-     </ul>
-     <h3>Lunch</h3>
-        <ul *ngFor='let meal2 of lunch'>
-          <li>{{meal2.name}}</li>
-          <li>{{meal2.details}}</li>
-          <li>{{meal2.calories}}</li>
-        </ul>
-      <h3>Diner</h3>
-       <ul *ngFor='let meal3 of diner'>
-         <li>{{meal3.name}}</li>
-         <li>{{meal3.details}}</li>
-         <li>{{meal3.calories}}</li>
-       </ul>
+      <label>Enter Food Details:</label>
+      <input [(ngModel)]="selectedFood.details">
+    </div>
     <div>
-
+    <label>Enter Food Name:</label>
+    <input [(ngModel)]="selectedFood.calories">
+    <button (click)="doneEditing()">Done</button>
+     </div>
+    </div>
+  </div>
+  </div>
   `
 })
 
 export class AppComponent {
-public breakfast: Food[]=[];
-public lunch: Food[]=[];
-public diner: Food[]=[];
-
-addName(breakfastname,breakfastdet,breakfastcal){
-  var newFoodItem=new Food(breakfastname,breakfastdet,breakfastcal)
-  this.breakfast.push(newFoodItem);
-  console.log("yes")
-
-}
-addLunch(lunchname,lunchdet,lunchcalc){
-  var newLunch =new Food(lunchname,lunchdet,lunchcalc)
-  this.lunch.push(newLunch);
-  console.log('lunch')
-}
-addDinner(dinername,dinerdet,dinnercalc){
-  var newDinner=new Food(dinername,dinerdet,dinnercalc)
-  this.diner.push(newDinner);
-  console.log('dinner')
-
-}
-
-
+  public meals: Food[] = [
+    new Food("Pilau","served hot with kachumbari",220),
+    new Food("Spaghetti","cooked with meatballs",370),
+    new Food("Chapati","soft chapatis",100),
+    new Food("Ugali","real energy-giving food",180),
+    new Food("Hamburger","extra cheese on the burger",400)
+  ];
+  selectedFood: Food = null;
+  showDetails(clickedFood: Food) {
+    this.selectedFood = clickedFood;
+  }
+  doneEditing() {
+   this.selectedFood = null;
+ }
 }
 
 export class Food{
+  public done: boolean = false;
   constructor(public name:string, public details:string, public calories:number){ }
 }
